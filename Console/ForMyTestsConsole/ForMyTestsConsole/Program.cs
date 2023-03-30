dc3609366ae53293930bee5bcb9ec223e5e7a975
EVGENYPC\Evgeny <es.zaharov@yandex.ru>
2023-03-30 21:48:51 +0300

    resolve conflitct

--- Console/ForMyTestsConsole/ForMyTestsConsole/Program.cs
+++ Console/ForMyTestsConsole/ForMyTestsConsole/Program.cs
@@ -1,9 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-<<<<<<< HEAD
 
 Console.WriteLine("main commit 1");
-=======
 Console.WriteLine("TaskProgramm1");
->>>>>>> TaskBranch1
+
