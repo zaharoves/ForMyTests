e9a7f8b6d375ef0be67590556483dbf36d4bdb04
EVGENYPC\Evgeny <es.zaharov@yandex.ru>
2023-03-30 22:09:42 +0300

    merge

--- Console/ForMyTestsConsole/ForMyTestsConsole/Program.cs
+++ Console/ForMyTestsConsole/ForMyTestsConsole/Program.cs
@@ -5,5 +5,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("main commit 1");
 Console.WriteLine("TaskProgramm1");
 
-
 Console.WriteLine("TaskProgramm 2 - 1 ");
+Console.WriteLine("TaskProgramm main - 1");
