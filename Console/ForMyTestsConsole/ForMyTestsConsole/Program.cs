50db2e131f3e167d1eecbad7c773ac5137f3db65
EVGENYPC\Evgeny <es.zaharov@yandex.ru>
2023-03-30 22:11:43 +0300

    main

--- Console/ForMyTestsConsole/ForMyTestsConsole/Program.cs
+++ Console/ForMyTestsConsole/ForMyTestsConsole/Program.cs
@@ -5,9 +5,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("main commit 1");
 Console.WriteLine("TaskProgramm1");
 
-<<<<<<< Updated upstream
 Console.WriteLine("TaskProgramm 2 - 1 ");
 Console.WriteLine("TaskProgramm main - 1");
-=======
-Console.WriteLine("TaskProgramm main - 1");
->>>>>>> Stashed changes
